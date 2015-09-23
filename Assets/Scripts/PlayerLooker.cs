93635a02755c3d806963919fea30a18081176c12
kuhnerdm <kuhnerdm@rose-hulman.edu>
2015-09-22 22:35:16 -0400

    Added game files and README.md

---
+++ Assets/Scripts/PlayerLooker.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerLooker : MonoBehaviour {
+
+	public float rotSpeed;
+
+	void Update () {
+		float rotX = Input.GetAxis ("Mouse X") * rotSpeed;
+		float rotY = -Input.GetAxis ("Mouse Y") * rotSpeed;
+		gameObject.transform.rotation = gameObject.transform.rotation * Quaternion.Euler(new Vector3(rotY, rotX, 0.0f));
+		float newRotX = gameObject.transform.rotation.eulerAngles.x;
+		float newRotY = gameObject.transform.rotation.eulerAngles.y;
+		gameObject.transform.rotation = Quaternion.Euler(new Vector3(newRotX, newRotY, 0));
+	}
+}
---
+++ Assets/Scripts/PlayerMover.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMover : MonoBehaviour {
+
+	public float speed;
+	public GameObject camera;
+
+	void Update() {
+		float moveX = Input.GetAxis ("Horizontal") * speed;
+		float moveZ = Input.GetAxis ("Vertical") * speed;
+		gameObject.transform.position = gameObject.transform.position + camera.transform.right * moveX * speed + camera.transform.forward * moveZ * speed;
+
+		float newPosX = gameObject.transform.position.x;
+		float newPosZ = gameObject.transform.position.z;
+		gameObject.transform.position = new Vector3 (newPosX, 3.0f, newPosZ);
+	}
+
+	void OnTriggerStay(Collider other) {
+		Vector3 vectorBetweenObjects = gameObject.transform.position - other.ClosestPointOnBounds (gameObject.transform.position);
+		gameObject.transform.position +=  vectorBetweenObjects / Mathf.Sqrt(vectorBetweenObjects.sqrMagnitude) * 0.01f;
+	}
+
+}
